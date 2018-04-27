c2a48789f677520413100f31a51d4b4276c2c2c7
Jacek Salamon <ajcek84@gmail.com>
2018-04-27 22:19:50 +0200

    Pierwszy komit

--- Manufaktura.RismCatalogue/ClientApp/app/app.shared.module.ts
+++ Manufaktura.RismCatalogue/ClientApp/app/app.shared.module.ts
@@ -7,15 +7,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { SearchResultsComponent } from './components/searchresults/searchresults.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
+        SearchResultsComponent,
         HomeComponent
     ],
     imports: [
@@ -25,8 +23,7 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'searchresults', component: SearchResultsComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
--- Manufaktura.RismCatalogue/ClientApp/app/components/searchresults/searchresults.component.ts
+++ Manufaktura.RismCatalogue/ClientApp/app/components/searchresults/searchresults.component.ts
@@ -2,10 +2,10 @@ import { Component, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
-    selector: 'fetchdata',
-    templateUrl: './fetchdata.component.html'
+    selector: 'search-results',
+    templateUrl: './searchresults.component.html'
 })
-export class FetchDataComponent {
+export class SearchResultsComponent {
     public forecasts: WeatherForecast[];
 
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
