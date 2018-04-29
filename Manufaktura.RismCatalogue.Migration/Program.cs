a1c516ff25951c2395ae39e6d2d8ec1a01af3e8d
Jacek Salamon <ajcek84@gmail.com>
2018-04-29 09:47:46 +0200

    Reading MarcXml - stub

--- Manufaktura.RismCatalogue.Migration/Program.cs
+++ Manufaktura.RismCatalogue.Migration/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Manufaktura.RismCatalogue.Model;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Xml;
 
 namespace Manufaktura.RismCatalogue.Migration
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = @"C:\Databases\rismAllMARCXMLexample\rism_130616_example.xml";
+            using (var db = new RismDbContext(new DbContextOptionsBuilder().UseMySql("server=localhost;database=manufaktura-rism;uid=admin;pwd=123123").Options))
+            {
+                using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = XmlReader.Create(fs))
+                    {
+                        while (reader.Read())
+                        {
+                           
+                        }
+                    }
+                }
+            }
         }
     }
 }
