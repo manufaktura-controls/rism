70649f9c41cd425ede2c7d88def67525103d1a73
Jacek Salamon <ajcek84@gmail.com>
2018-04-29 18:06:12 +0200

    Params

--- Manufaktura.RismCatalogue/ClientApp/app/components/searchresults/searchresults.component.ts
+++ Manufaktura.RismCatalogue/ClientApp/app/components/searchresults/searchresults.component.ts
@@ -9,7 +9,11 @@ export class SearchResultsComponent {
     public searchResults: SearchResult[] = [];
 
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        http.get(baseUrl + 'api/Search/Search').subscribe(result => {
+        http.get(baseUrl + 'api/Search/Search', {
+            params: {
+                skip: 0, 
+                take: 20
+            }}).subscribe(result => {
             var resultsPage = result.json() as SearchResult[];
             for (var r in resultsPage) this.searchResults.push(resultsPage[r]);
         }, error => console.error(error));
