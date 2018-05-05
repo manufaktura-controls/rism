2f6034fe3be71c67798893a108cc629ad85077f4
Jacek Salamon <ajcek84@gmail.com>
2018-05-05 20:11:18 +0200

    Knockout project

---
+++ Manufaktura.RismCatalogue.Knockout/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Manufaktura.RismCatalogue.Knockout.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
