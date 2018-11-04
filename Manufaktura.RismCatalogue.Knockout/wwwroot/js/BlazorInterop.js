7b3f3805819bd563584b44981710a41af0375d64
Jacek Salamon <ajcek84@gmail.com>
2018-11-04 17:36:57 +0100

    Determining number of piano keys according to container size

---
+++ Manufaktura.RismCatalogue.Knockout/wwwroot/js/BlazorInterop.js
@@ -0,0 +1,9 @@
+﻿
+function registerKeyboard(keyboard) {
+    window.incipitKeyboard = keyboard;
+}
+
+$(window).resize(function () {
+    if (!window.incipitKeyboard) return;
+    window.incipitKeyboard.invokeMethod('OnWindowResize', $("#searchPanel").width());
+});
