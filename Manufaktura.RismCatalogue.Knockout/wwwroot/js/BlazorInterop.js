0c86c67a904e5848c1cf3983a2901fc1df4c2c0d
Jacek Salamon <ajcek84@gmail.com>
2018-11-06 00:04:19 +0100

    Note viewer control for entering search criteria

--- Manufaktura.RismCatalogue.Knockout/wwwroot/js/BlazorInterop.js
+++ Manufaktura.RismCatalogue.Knockout/wwwroot/js/BlazorInterop.js
@@ -1,6 +1,15 @@
 ﻿
 function registerKeyboard(keyboard) {
     window.incipitKeyboard = keyboard;
+
+    $(".btn-key").click(function (event) {
+        var pitch = $(event.currentTarget).attr("midiPitch");
+        window.noteViewer.invokeMethod('AddNote', pitch);
+    });
+}
+
+function registerNoteViewer(noteViewer) {
+    window.noteViewer = noteViewer;
 }
 
 $(window).resize(function () {
